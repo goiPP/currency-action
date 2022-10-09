f7f7f5851d839aa12eebaffab7ca9f4f88647ad4
PATCHARIDA PUDPADEE <calojy@hotmail.com>
2022-10-09 17:59:49 +0200

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
     output := action.GetInput("output")
     if output == "response" {
-        action.infof(currency.ExchangeResponse(to_currency, from_currency)
+	    action.infof(currency.ExchangeResponse(to_currency, from_currency))
     } 
 
 }
